08b61f3984f712e63166ac6a49aa9ea29b505376
joshuapondrom <japrkf@mst.edu>
2017-10-26 07:38:13 -0500

    Added more invariants

--- knap.py
+++ knap.py
@@ -5,14 +5,20 @@ def Knapsack(weightcap, weights, values, n):
     #Initializes our List with 0's when at 0 weight
     V = [[0 for x in range(weightcap+1)] for x in range(n+1)]
     print(np.matrix(V))
-    #Main loop to find the 
+    #Main loop to find what will be in the knapsack
     for i in range(n+1):
         for j  in range(weightcap+1):
+            #If we are at weight at zero
             if i == 0 or j == 0:
                 V[i][j] = 0
+            #If we have room for the weight, we will take the max
+            # of either having that weight or having the other
+            # elements that will fit ( the array index above it )
             elif weights[i-1] <= j:
                 V[i][j] = max(values[i-1] + V[i-1][j-weights[i-1]],
                               V[i-1][j])
+            #If it will not fit, it will be equal to the previously
+            # calculated max with not having the current item
             else:
                 V[i][j] = V[i-1][j]
     print(np.matrix(V))
